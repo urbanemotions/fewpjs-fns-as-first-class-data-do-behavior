f7a079afc2928c2d9e45178f6200bfcf3be05b79
sin wan wildy <sw.wildy@gmail.com>
2021-02-23 11:01:55 -0800

    completed

--- index.js
+++ index.js
@@ -10,4 +10,25 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(timeStr)
+{
+  const timeHour = parseInt(timeStr, 10); 
+  /* radix of 10 converts from a decimal number */
+  if (timeHour < 12) 
+  {
+    return "Good Morning"
+  } else if (timeHour > 17) 
+  {
+    return "Good Evening"
+  }
+  else 
+  {
+    return "Good Afternoon"
+  }
+}
+
 /* Write your implementation of displayMessage() */
+function displayMessage(msgStr)
+{
+  document.getElementById("greeting").innerText = msgStr;
+}
